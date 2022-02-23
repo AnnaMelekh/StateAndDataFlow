8b596c9e4cef36575c4f8eff25ec9f29df0b96f6
AnnaMelekh <annasmelekhina@gmail.com>
2022-02-23 17:11:34 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Anna Melekhina on 23.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
